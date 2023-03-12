33ad7b8e02e5b1dcd306738aa86be2d77274d336
muhammad-taufik-ramadhan <mtaufikrama14@gmail.com>
2023-03-12 18:11:57 +0700

    nemu solusi url_strategy

--- lib/app/modules/portfolio/controllers/portfolio_controller.dart
+++ lib/app/modules/portfolio/controllers/portfolio_controller.dart
@@ -1,13 +1,7 @@
 import 'package:get/get.dart';
 
 class PortfolioController extends GetxController {
-  String id = '0';
-  String lang = Get.deviceLocale!.languageCode;
-
-  @override
-  void onInit() {
-    id = Get.parameters['id'] ?? '0';
-    lang = Get.parameters['lang'] ?? Get.deviceLocale!.languageCode;
-    super.onInit();
-  }
+  final String id = Get.parameters['id'] ?? '0';
+  final String lang = Get.parameters['lang'] ??
+      (Get.deviceLocale != null ? Get.deviceLocale!.languageCode : 'en');
 }
